74523c9425f1d4f04e1a0ecc06bddf71681e85b7
Nishil Anand <88177986+nishoof@users.noreply.github.com>
2024-09-17 16:08:58 -0700

    Bot replies to certain messages with a counter

--- src/index.js
+++ src/index.js
@@ -2,19 +2,41 @@ require("dotenv").config();
 
 const { Client, IntentsBitField } = require('discord.js');
 const TOKEN = process.env.TOKEN
+let botId = '';
+let petCounter = 0;
 
 // Defining client with the discord intents (https://discord.com/developers/docs/topics/gateway#list-of-intents)
 const client = new Client({
     intents: [
+        IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent,
     ]
 });
 
+
+// LISTENERS 
+
 // Listens for whenever the bot is ready
 client.on('ready', (c) => {
-    console.log(`${c.user.tag} is online!`)
+    console.log(`${c.user.tag} is online!`);
+    botId = c.user.id;
 });
 
-// Login, actually running the bot
+// Listens for new messages in the server
+client.on(`messageCreate`, (message) => {
+    // console.log(message);
+
+    let senderId = message.author.id;
+    if (senderId == botId) return;              // ignore messages sent by this bot
+    
+    if (message.content == 'pet faadil') {
+        message.reply('faadil has been pet ' + ++petCounter + ' times');
+    }
+});
+
+
+// LOGIN
+
+// Login (actually running the bot)
 client.login(TOKEN);
